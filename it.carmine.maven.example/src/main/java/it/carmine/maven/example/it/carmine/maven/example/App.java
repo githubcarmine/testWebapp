ffffc12de987db2ed6a4ef811c1372dd7b4ba082
yyi2096 <yyi2096@IC1300053.ic.intra.infocamere.it>
2019-04-01 12:56:27 +0200

    first commit;

---
+++ it.carmine.maven.example/src/main/java/it/carmine/maven/example/it/carmine/maven/example/App.java
@@ -0,0 +1,13 @@
+package it.carmine.maven.example.it.carmine.maven.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ it.carmine.maven.example/src/main/java/it/carmine/maven/example/it/carmine/maven/example/Util.java
@@ -0,0 +1,10 @@
+package it.carmine.maven.example.it.carmine.maven.example;
+
+public class Util {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
