d26fcab9a28e0ae8c63083901983cab5b4f8992d
yyi2096 <yyi2096@IC1300053.ic.intra.infocamere.it>
2019-04-01 13:09:21 +0200

    nuovo progetto

---
+++ carmineSecondo/src/main/java/it/carmine/maven/example/carmineSecondo/App.java
@@ -0,0 +1,13 @@
+package it.carmine.maven.example.carmineSecondo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
