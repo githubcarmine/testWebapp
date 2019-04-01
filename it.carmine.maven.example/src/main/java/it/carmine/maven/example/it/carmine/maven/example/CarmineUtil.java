5222b9b9f42320e7ba97418621ee60a928e230b0
yyi2096 <yyi2096@IC1300053.ic.intra.infocamere.it>
2019-04-01 13:01:24 +0200

    Secondo commit;

---
+++ it.carmine.maven.example/src/main/java/it/carmine/maven/example/it/carmine/maven/example/CarmineUtil.java
@@ -0,0 +1,10 @@
+package it.carmine.maven.example.it.carmine.maven.example;
+
+public class CarmineUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
